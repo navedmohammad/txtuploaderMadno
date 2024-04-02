bf93a8744977baba09946e05857903ad2c551ee9
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-04-02 13:13:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
-api_id = 28912814
-api_hash = "8c69efa7804f6f3eb167984e2f4cf549"
-bot_token = "6647623608:AAEam-VeKvC4I0mbaaXwym15j0hw3l0Wtes"
-# Bot Username = "@jaishriramtxtBot"
+api_id = 20483216
+api_hash = "2518170d3dd939b3f2893cb0aae805c4"
+bot_token = "6856442446:AAFrHW9bHBUenHmzzQe9Y2rvujju882Sxbs"
+# Bot Username = "@Madno_bot"
