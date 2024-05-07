860dec9667a70bfc26cf2e3135259c3c309d493a
navedmohammad <36394261+navedmohammad@users.noreply.github.com>
2024-05-07 14:19:40 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,4 @@
 api_id = 20483216
 api_hash = "2518170d3dd939b3f2893cb0aae805c4"
-bot_token = "6856442446:AAFrHW9bHBUenHmzzQe9Y2rvujju882Sxbs"
-# Bot Username = "@Madno_bot"
+bot_token = "6985837119:AAGGO8oIASJELHvGfK3YN-RvCMnS5ig6C7Q"
+# Bot Username = "@Aslam"
