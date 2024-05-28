dfde0e373109984040b9be572744d10f46486e18
Mohammadnaved Naved <36394261+navedmohammad@users.noreply.github.com>
2024-05-28 12:24:22 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,4 +1,13 @@
-api_id = 20483216
-api_hash = "2518170d3dd939b3f2893cb0aae805c4"
-bot_token = "6985837119:AAGGO8oIASJELHvGfK3YN-RvCMnS5ig6C7Q"
-# Bot Username = "@Aslam"
+#𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝙎𝙢𝙚𝙭𝙮 ᥫ᭡](https://t.me/SmexyOP)
+
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+
+
+
+api_id = "28619451"
+
+api_hash = "e41e306cd0e0d0be11ec78c281f0b628"
+
+bot_token = "7084783725:AAFqfIvmE25YPJ_rliHaNES6a5Xxmsq3Vsc"
+
+# Bot Username = "https://t.me/SmexyTxtLeechingBot"
